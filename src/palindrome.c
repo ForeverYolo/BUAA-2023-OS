791ae8751bfd0d451d27ad4bb581fd14dbc7e591
Unicorn <2572131118@qq.com>
2023-02-27 19:13:27 +0800

    palindrome提交

--- src/palindrome.c
+++ src/palindrome.c
@@ -2,8 +2,13 @@
 int main() {
 	int n;
 	scanf("%d", &n);
-
-	if () {
+	int x = n;
+	int new = 0;
+	do {
+		new = new*10 + x%10;
+		x = x/10;
+	} while(x!=0)
+	if (new == n) {
 		printf("Y\n");
 	} else {
 		printf("N\n");
