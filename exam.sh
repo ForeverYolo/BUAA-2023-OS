b316b13096357621fa1b41e7fb104516b5ea0dce
Unicorn <2572131118@qq.com>
2023-03-06 19:32:47 +0800

    第一次提交

---
+++ exam.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+
+mkdir mydir
+chmod +x ./mydir
+chmod +r ./mydir
+chmod +w ./mydir
+touch ./mydir/myfile
+echo 2023 > ./mydir/myfile
+mv moveme ./mydir
+cp copyme ./mydir
+mv ./mydir/copyme ./mydir/copied
+cat readme
+gcc bad.c 2>err.txt
+
+
+mkdir gen
+if [ $# -eq 1 ] 
+then
+	
+	for i in $(seq 1 $1)
+	do
+		touch ./gen/$i.txt
+	done
+fi
+
+if [ $# -eq 0 ]
+then
+	for j in {1..10}
+	do
+		touch ./gen/$j.txt
+	done
+fi
+
+
