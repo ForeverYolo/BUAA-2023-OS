f9b93614a9daa1856dda67b90c48f0f950d32bcf
任航麒 <21373271@buaa.edu.cn>
2023-04-16 13:24:24 +0000

    第一次提交

--- kern/pmap.c
+++ kern/pmap.c
@@ -429,6 +429,7 @@ void physical_memory_manage_check(void) {
 	printk("physical_memory_manage_check() succeeded\n");
 }
 
+
 void page_check(void) {
 	Pde *boot_pgdir = alloc(BY2PG, BY2PG, 1);
 	struct Page *pp, *pp0, *pp1, *pp2;
@@ -537,4 +538,135 @@ void page_check(void) {
 	page_free(pp2);
 
 	printk("page_check() succeeded!\n");
+}
+
+
+#include <swap.h>
+
+struct Page_list page_free_swapable_list;
+static u_char *disk_alloc();
+static void disk_free(u_char *pdisk);
+
+void swap_init() {
+	LIST_INIT(&page_free_swapable_list);
+	for (int i = SWAP_PAGE_BASE; i < SWAP_PAGE_END; i += BY2PG) {
+		struct Page *pp = pa2page(i);
+		LIST_REMOVE(pp, pp_link);
+		LIST_INSERT_HEAD(&page_free_swapable_list, pp, pp_link);
+	}
+}
+
+// Interface for 'Passive Swap Out'
+struct Page *swap_alloc(Pde *pgdir, u_int asid) {
+	// Step 1: Ensure free page
+	if (LIST_EMPTY(&page_free_swapable_list)) {
+		/* Your Code Here (1/3) */
+		int pa = 0x3900000;
+	       	struct Page * p = pa2page(pa);
+		u_char * da = disk_alloc();
+		u_int pageda = ((u_int)da / BY2PG) << 12;
+		for (int i = 0;i < 1024; i++) {
+			Pde *pgdir_entryp = pgdir + i;
+			if ((*pgdir_entryp & PTE_V)) {
+				Pte* pte = (Pte*)KADDR(PTE_ADDR(*pgdir_entryp));
+				for (int j = 0; j < 1024 ; j++) {
+					Pte* pte_entry = pte + j;
+					if ( *pte_entry & PTE_V ) {
+						if ( PTE_ADDR(*pte_entry) == page2pa(p) )
+						{
+							*pte_entry = (*pte_entry & ~PTE_V) | PTE_SWP;
+							*pte_entry = *pte_entry & 0xFFF;
+							*pte_entry = pageda | *pte_entry;
+							u_int va = i << 22 |  j << 12;  
+							tlb_invalidate(asid, va);
+						}
+					}
+				}
+			}
+		}
+		memcpy((void*)da, (void*)page2kva(p), BY2PG);
+		LIST_INSERT_HEAD( &page_free_swapable_list,p,pp_link);
+	}
+
+	// Step 2: Get a free page and clear it
+	struct Page *pp = LIST_FIRST(&page_free_swapable_list);
+	LIST_REMOVE(pp, pp_link);
+	memset((void *)page2kva(pp), 0, BY2PG);
+
+	return pp;
+}
+
+// Interfaces for 'Active Swap In'
+static int is_swapped(Pde *pgdir, u_long va) {
+	Pte *pte;
+	/* Step 1: Get corresponding page table entry. */
+	pgdir_walk(pgdir, va, 0, &pte);
+	if (pte && (*pte & (PTE_SWP | PTE_V)) == PTE_SWP) {
+		return 1;
+	}
+	return 0;
+}
+
+static void swap(Pde *pgdir, u_int asid, u_long va) {
+	/* Your Code Here (3/3) */
+	struct Page * p  = swap_alloc(pgdir,asid);
+	va = ROUNDDOWN(va,BY2PG);
+	Pte * pte_va;
+	pgdir_walk(pgdir,va,0,&pte_va);
+	u_char* da =(u_char*)((PTE_ADDR(*pte_va) >> 12) * BY2PG);
+	memcpy((void*)page2kva(p),da,BY2PG);
+	for (int i = 0;i < 1024; i++) {
+		Pde *pgdir_entryp = pgdir + i;
+		if ((*pgdir_entryp & PTE_V)) {
+			Pte* pte = (Pte*)KADDR(PTE_ADDR(*pgdir_entryp));
+			for (int j = 0; j < 1024 ; j++) {
+				Pte* pte_entry = pte + j;
+				if ( (*pte_entry & (PTE_V | PTE_SWP)) == PTE_SWP ) {
+					if ( PTE_ADDR(*pte_entry) == PTE_ADDR(*pte_va) )
+					{
+						*pte_entry = (*pte_entry | PTE_V) & ~PTE_SWP;
+						*pte_entry = *pte_entry & 0xFFF;
+						*pte_entry = page2pa(p) | *pte_entry;
+					      	u_int va = i << 22 |  j << 12;	
+						tlb_invalidate(asid, va);
+					}
+				}
+			}
+		}
+	}
+	disk_free(da);
+}
+
+Pte swap_lookup(Pde *pgdir, u_int asid, u_long va) {
+	// Step 1: If corresponding page is swapped out, swap it in
+	if (is_swapped(pgdir, va)) {
+		swap(pgdir, asid, va);
+	}
+
+	// Step 2: Look up page table element.
+	Pte *ppte;
+	page_lookup(pgdir, va, &ppte);
+
+	// Step 3: Return
+	return ppte == NULL ? 0 : *ppte;
+}
+
+// Disk Simulation (Do not modify)
+u_char swap_disk[SWAP_DISK_NPAGE * BY2PG] __attribute__((aligned(BY2PG)));
+u_char swap_disk_used[SWAP_DISK_NPAGE];
+
+static u_char *disk_alloc() {
+	int alloc = 0;
+	for (;alloc < SWAP_DISK_NPAGE && swap_disk_used[alloc]; alloc++) {
+		;
+	}
+	assert(alloc < SWAP_DISK_NPAGE);
+	swap_disk_used[alloc] = 1;
+	return &swap_disk[alloc * BY2PG];
+}
+
+static void disk_free(u_char *pdisk) {
+	int offset = pdisk - swap_disk;
+	assert(offset % BY2PG == 0);
+	swap_disk_used[offset / BY2PG] = 0;
 }
