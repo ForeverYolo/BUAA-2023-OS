78254a4e6fe52368be3c253d85cf77e6768b4c32
Unicorn <2572131118@qq.com>
2023-05-18 19:31:43 +0800

    first

--- user/sh.c
+++ user/sh.c
@@ -88,8 +88,13 @@ int parsecmd(char **argv, int *rightpipe) {
 			}
 			// Open 't' for reading, dup it onto fd 0, and then close the original fd.
 			/* Exercise 6.5: Your code here. (1/3) */
-
-			user_panic("< redirection not implemented");
+			if ( (r = open(t,O_RDONLY)) < 0 ) {
+				return r;
+			}
+			fd = r;
+			dup(fd,0);
+			close(fd);
+			//user_panic("< redirection not implemented");
 
 			break;
 		case '>':
@@ -99,9 +104,13 @@ int parsecmd(char **argv, int *rightpipe) {
 			}
 			// Open 't' for writing, dup it onto fd 1, and then close the original fd.
 			/* Exercise 6.5: Your code here. (2/3) */
-
-			user_panic("> redirection not implemented");
-
+			if ( (r = open(t,O_WRONLY)) < 0 ) {
+				return r;
+			}
+			fd = r;
+			dup(fd,1);
+			close(fd);
+			//user_panic("> redirection not implemented");
 			break;
 		case '|':;
 			/*
@@ -121,13 +130,24 @@ int parsecmd(char **argv, int *rightpipe) {
 			 */
 			int p[2];
 			/* Exercise 6.5: Your code here. (3/3) */
-
-			user_panic("| not implemented");
-
+			if ( (r = pipe(p)) < 0 ) {
+				return r;
+			}
+			if ( ((*rightpipe) = fork()) == 0 ) {
+				dup(p[0],0);
+				close(p[0]);
+				close(p[1]);
+				return parsecmd(argv,rightpipe);
+			} else {
+				dup(p[1],1);
+				close(p[0]);
+				close(p[1]);
+				return argc;
+			}
+			//user_panic("| not implemented");
 			break;
 		}
 	}
-
 	return argc;
 }
 
